f2c10d4f4215cdee907b0d541fe15ae81f395933
Homlesnurr <abel.yttervik@gmail.com>
2023-02-04 00:45:54 +0100

    man

--- Bezier_curve.py
+++ Bezier_curve.py
@@ -3,54 +3,73 @@ import numpy as np
 
 class point():
     def __init__(self, x, y, father = None, depth = 0):
+        self.t = 0
         self.x = x
         self.y = y
         self.father = father
         self.depth = depth + 1
         
+        
+    def movePoint(self, dt):             
+        self.x = (1 - self.t) * self.father.startPoint.x + self.t * self.father.endPoint.x
+        self.y = (1 - self.t) * self.father.startPoint.y + self.t * self.father.endPoint.y
+        
+    
     def __repr__(self):
-        return f"({self.x},{self.y}, on {self.father}, depth {self.depth})"
+        return f"({self.x},{self.y}) on {self.father}, depth {self.depth}"
 
 class vector():
-    def __init__(self, sx, sy, ex, ey, depth = 0, father = None):
-        self.x = sx
-        self.y = sy
+    def __init__(self, startPoint, endPoint, depth = 0):
+        self.startPoint = startPoint
+        self.endPoint = endPoint
         
-        self.endx = ex
-        self.endy = ey
-        
-        self.dx = ex-sx
-        self.dy = ey-sy
+        self.startX = startPoint.x
+        self.startY = startPoint.y
         
         self.depth = depth + 1
-        self.father = father
         
     def __repr__(self):
         return f"({self.x},{self.y}) -> ({self.endx},{self.endy})"
     
 
-def movePoint(mPoint):
-    pass
+
 
 def bezier(allPoints):
     vectors = []
     vectors.append(getVectors(allPoints))
     
+    xvalues = []
+    yvalues = []
+    
+    dt = 0.001
+    t = int(1/dt)
+    
     for i in range(len(allPoints) - 2):
         nextSetOfPoints = []
         for v in vectors[i]:
-            pOV = point(v.x, v.y, v, v.depth)
+            pOV = point(v.startX, v.startY, v, v.depth)
             nextSetOfPoints.append(pOV)
         
         vectors.append(getVectors(nextSetOfPoints))
-        
-    return vectors
+    
+    for i in range(t):
+        for i in range(1, len(vectors)):
+            if len(vectors[i]) == 1:
+                xvalues.append(vectors[i][0].startPoint.x)
+                yvalues.append(vectors[i][0].startPoint.y)
+            else:
+                for v in vectors[i]:
+                    v.startPoint.movePoint(dt)
+                    v.endPoint.movePoint(dt)
+                
+    
+    return xvalues, yvalues
 
 def getVectors(points):
     output = []
     
     for i,p in enumerate(points[:-1]):
-        output.append(vector(p.x, p.y, points[i+1].x, points[i+1].y, p.depth, p.father))
+        output.append(vector(p, points[i+1], p.depth))
     
     return output
 
@@ -65,7 +84,14 @@ midPoint2 = point(2,0)
 allPoints = [startPoint,midPoint1,midPoint2,endPoint]
 midPoints = allPoints[1:-1]
 
-for i in range(len(bezier(allPoints))):
-    print('\n',i)
-    for p in bezier(allPoints)[i]:
-        print(p.father) if p.father else print(p)
+curve = bezier(allPoints)
+
+xvalues = []
+yvalues = []
+
+for p in allPoints:
+    xvalues.append(p.x)
+    yvalues.append(p.y)
+
+plt.plot(xvalues, yvalues)
+plt.show()
